f3b302c741733e79b72efe0a3a8de70855629e1c
ChetahPangestu <1001878@student.sae.edu.au>
2016-02-20 18:24:23 +1100

    D4C

---
+++ [SAE]Puzzle Maze/Source/Assets/Countdown.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Countdown : MonoBehaviour {
+
+    public float time = 50.0f;
+    public Text secondsLeftText;
+    private string timeLeftString;
+    public string loseScene;
+    
+    // Use this for initialization
+    void Start ()
+    {
+      
+	}
+    public void Update()
+    {
+
+        time += Time.deltaTime;
+
+        //if (timeLeft <= 0.0f)
+        //{
+        //    SceneManager.LoadScene(loseScene);
+        //}
+        timeLeftString = time.ToString("F0");
+        secondsLeftText.text = timeLeftString;
+    }
+}
